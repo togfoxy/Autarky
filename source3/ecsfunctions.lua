0c9111e3574224966b09433b822e808f14832e1d
Fox <g_t_campbell@yahoo.com.au>
2022-05-04 21:55:16 +1000

    Tiles and contours draw properly

--- source3/ecsfunctions.lua
+++ source3/ecsfunctions.lua
@@ -17,6 +17,7 @@ function ecsfunctions.init()
 
     -- define same systems
     function systemDraw:draw()
+
         love.graphics.setColor(1,1,1,1)
         for _, e in ipairs(self.pool) do
             if e.isTile then
@@ -26,9 +27,56 @@ function ecsfunctions.init()
                 local imageheight = img:getHeight()
                 local drawscalex = (TILE_SIZE / imagewidth)
                 local drawscaley = (TILE_SIZE / imageheight)
+                local offsetx = imagewidth / 2
+                local offsety = imageheight / 2
+
 				love.graphics.setColor(1,1,1,1)
-                love.graphics.draw(img, drawx, drawy, 0, drawscalex, drawscaley)
+                love.graphics.draw(img, drawx, drawy, 0, drawscalex, drawscaley, offsetx, offsety)
+
+                -- debugging
+                -- love.graphics.circle("line", drawx, drawy, 3)
+                -- love.graphics.print(e.isTile.tileType, drawx, drawy)
+                -- love.graphics.print(e.isTile.tileHeight, drawx, drawy)
+
+                -- draw contour lines
+                local row, col = e.position.row, e.position.col
+                -- check if top neighbour is different to current cell
+                if row > 1 then
+                    if MAP[row-1][col].height ~= MAP[row][col].height then
+                        -- draw line
+                        local x1, y1 = fun.getXYfromRowCol(row, col)
+                        local x2, y2 = x1 + TILE_SIZE, y1
+                        local alpha = MAP[row][col].height / UPPER_TERRAIN_HEIGHT
+                        x1 = x1 - (TILE_SIZE / 2)
+                        y1 = y1 - (TILE_SIZE / 2)
+                        x2 = x2 - (TILE_SIZE / 2)
+                        y2 = y2 - (TILE_SIZE / 2)
+                        love.graphics.setColor(1,1,1,alpha)
+                        love.graphics.line(x1, y1, x2, y2)
+                    end
+                end
+
+                -- left side
+                if col > 1 then
+                    if MAP[row][col-1].height ~= MAP[row][col].height then
+                        -- draw line
+                        local x1, y1 = fun.getXYfromRowCol(row, col)
+                        local x2 = x1
+                        local y2 = y1 + TILE_SIZE
+                        local alpha = MAP[row][col].height / UPPER_TERRAIN_HEIGHT
+                        x1 = x1 - (TILE_SIZE / 2)
+                        y1 = y1 - (TILE_SIZE / 2)
+                        x2 = x2 - (TILE_SIZE / 2)
+                        y2 = y2 - (TILE_SIZE / 2)
+
+
+                        love.graphics.setColor(1,1,1,alpha)
+                        love.graphics.line(x1, y1, x2, y2)
+                    end
+                end
             end
+
+
             if e.isPerson then
                 if e.isSelected then
                     love.graphics.setColor(0,1,0,1)
@@ -37,9 +85,6 @@ function ecsfunctions.init()
                 end
                 local drawwidth = PERSON_DRAW_WIDTH
                 local drawx, drawy = e.position.x, e.position.y
-                local offsetx, offsety = TILE_SIZE / 2, TILE_SIZE / 2
-                drawx = drawx + offsetx
-                drawy = drawy + offsety
                 love.graphics.circle("fill", drawx, drawy, drawwidth)
             end
         end
@@ -53,7 +98,7 @@ function ecsfunctions.init()
         self.pool.onEntityAdded = function(_, entity)
             local row = entity.position.row
             local col = entity.position.col
-            MAP[row][col] = entity
+            -- MAP[row][col] = entity
         end
         --self.poolB.onEntityAdded = function(_, entity)
         --    table.insert(VILLAGERS, entity)
@@ -81,6 +126,8 @@ function ecsfunctions.init()
             -- the noise function only works with numbers between 0 and 1
             MAP[row][col].height = cf.round(love.math.noise(rowvalue, colvalue, terrainheightperlinseed) * UPPER_TERRAIN_HEIGHT)
             MAP[row][col].tileType = cf.round(love.math.noise(rowvalue, colvalue, terraintypeperlinseed) * 4)
+
+-- print(row, col, MAP[row][col].height)
 
             local TILES = concord.entity(WORLD)
             :give("drawable")
