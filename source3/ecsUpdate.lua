a89ea7bc6f4afda8d24a05a00fd86075569d923b
Fox <g_t_campbell@yahoo.com.au>
2022-06-25 13:27:55 +1000

    Fixes #240

--- source3/ecsUpdate.lua
+++ source3/ecsUpdate.lua
@@ -216,7 +216,7 @@ local function getNewGoal(villager)
                             fun.createActions(enum.goalBuyWood, villager)
                         else
                             fun.createActions(enum.goalWork, villager)
-                            print("golf")
+                            print("golf - try to work")
                         end
                     else    -- has wood
                         if villager.isPerson.stockInv[enum.stockWood] > 0 then
